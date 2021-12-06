a28cdb604a064b127c0ea0d1562604c515baf594
Nhat Nguyen Hong <nhat.nguyenhong@gameloft.com>
2021-12-06 08:56:50 +0700

    update

---
+++ time.cpp
@@ -0,0 +1,51 @@
+#include "time.h"
+
+#include <Windows.h>
+
+namespace floral
+{
+// ----------------------------------------------------------------------------
+
+#ifdef FLORAL_PLATFORM_WINDOWS
+struct windows_time_t
+{
+	LARGE_INTEGER perfFreq;
+	LARGE_INTEGER startTime;
+};
+
+typedef windows_time_t platform_time_t;
+#else
+// TODO
+#endif
+
+static platform_time_t initialize_time();
+static platform_time_t s_platformTime = initialize_time();
+
+// ----------------------------------------------------------------------------
+
+#ifdef FLORAL_PLATFORM_WINDOWS
+platform_time_t initialize_time()
+{
+	platform_time_t platformTime;
+	QueryPerformanceFrequency(&platformTime.perfFreq);
+	QueryPerformanceCounter(&platformTime.startTime);
+
+	return platformTime;
+}
+
+const f32 get_time_absolute()
+{
+	LARGE_INTEGER currentTime;
+	QueryPerformanceCounter(&currentTime);
+	INT64 ticks = currentTime.QuadPart - s_platformTime.startTime.QuadPart;
+
+	f32 absTime = (f32)ticks / (f32)s_platformTime.perfFreq.QuadPart;
+	absTime *= 1000.0f;
+	return absTime;
+}
+#else
+// TODO
+#endif
+
+// ----------------------------------------------------------------------------
+}

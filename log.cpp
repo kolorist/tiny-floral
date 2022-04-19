22944b52caea2aecf5edab765e0a4c45673876ee
Nhat Nguyen Hong <nhat.nguyenhong@gameloft.com>
2022-04-19 12:25:13 +0700

    minor

--- log.cpp
+++ log.cpp
@@ -16,32 +16,43 @@ void logf(log_level_e i_logLevel, const_cstr i_fmt, ...)
 {
     c8 str[4096];
     s32 p = 0;
+    s32 rLen = 4000;
 
     switch (i_logLevel)
     {
         case log_level_e::verbose:
-            p = sprintf(&str[0], "[verbose] ");
+            p = sprintf_s(&str[0], rLen, "[verbose] ");
             break;
         case log_level_e::debug:
-            p = sprintf(&str[0], "[debug] ");
+            p = sprintf_s(&str[0], rLen, "[debug] ");
             break;
         case log_level_e::info:
-            p = sprintf(&str[0], "[info] ");
+            p = sprintf_s(&str[0], rLen, "[info] ");
             break;
         case log_level_e::warning:
-            p = sprintf(&str[0], "[warning] ");
+            p = sprintf_s(&str[0], rLen, "[warning] ");
             break;
         case log_level_e::error:
-            p = sprintf(&str[0], "[error] ");
+            p = sprintf_s(&str[0], rLen, "[error] ");
             break;
         default:
             break;
     }
 
+    rLen -= p;
+
     va_list args;
     va_start(args, i_fmt);
-    vsprintf(&str[p], i_fmt, args);
+    rLen -= vsprintf_s(&str[p], rLen, i_fmt, args);
     va_end(args);
+
+    if (rLen == 0)
+    {
+        str[4000] = '.';
+        str[4001] = '.';
+        str[4002] = '.';
+        str[4003] = 0;
+    }
 
     puts(str);
     strcat(str, "\n");
